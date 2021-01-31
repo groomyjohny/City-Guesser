37ab31d11691ab93fa3590c23f18889efb5e7665
groomyjohny <groomyjohny@gmail.com>
2021-01-31 14:56:38 +0500

    Moved GameInstance into it's own file

--- main.py
+++ main.py
@@ -1,72 +1,11 @@
-from cities import cities
-from flask import Flask, render_template, redirect, request, abort
 from apikey import apikey
-import random
+from flask import Flask, render_template, redirect, request, abort
 import requests
-staticApiAddr = "https://static-maps.yandex.ru/1.x/?"
+from GameInstance import GameInstance
+from shared import *
+
+
 games = {}
-
-class GameInstance:
-    def __init__(self) -> None:
-        self.hint = []
-        self.reset()
-        pass
-
-    def reset(self):        
-        slidePaths = []
-        chosenCity =  random.choice(cities)
-        hint = cities
-        hintNot = []
-        cityCoords = fetch_coordinates(apikey,chosenCity)
-        cityCoords = [ float(cityCoords[0]), float(cityCoords[1]) ]
-        hintsUsed = 0
-
-        slideCount = 10
-        for i in range(0,slideCount):
-            zoom = int(random.uniform(13,17))
-            offset = [ random.uniform(-0.05,0.05), random.uniform(-0.05,0.05) ]
-            pos = [ cityCoords[0] + offset[0], cityCoords[1] + offset[1] ]
-            mtype = random.choice(['map','sat'])
-            args = {
-                'l' : mtype,
-                'll' : f"{pos[0]},{pos[1]}",
-                'z' : str(zoom)
-            }
-            slidePath = staticApiAddr + encodeParams(args)
-            slidePaths.append(slidePath)
-        
-        self.slidePaths = slidePaths
-        self.chosenCity = chosenCity
-        self.hint = hint
-        self.hintNot = hintNot
-        self.cityCoords = cityCoords
-        self.hintsUsed = hintsUsed
-        pass
-
-    def getHint(self):
-        if len(self.hint) > 2:
-            el = random.choice(self.hint)
-            self.hintNot.append(el)
-            self.hint.remove(el)
-            self.hintsUsed += 1
-            pass
-    
-
-def fetch_coordinates(apikey, place):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    params = {"geocode": place, "apikey": apikey, "format": "json"}
-    response = requests.get(base_url, params=params)
-    response.raise_for_status()
-    places_found = response.json()['response']['GeoObjectCollection']['featureMember']
-    most_relevant = places_found[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
-
-def encodeParams(args):
-    p = ''
-    for k in args:
-        p += k + '=' + args[k] + '&'
-    return p
 
 app = Flask(__name__)
 
