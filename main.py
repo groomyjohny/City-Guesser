73e5dc84b84de5864e5512b86e847021050c71d4
groomyjohny <groomyjohny@gmail.com>
2021-01-31 13:59:03 +0500

    class stash

--- main.py
+++ main.py
@@ -1,11 +1,55 @@
 from cities import cities
-staticApiAddr = "https://static-maps.yandex.ru/1.x/?"
-games = {}
-
 from flask import Flask, render_template, redirect, request, abort
 from apikey import apikey
 import random
 import requests
+staticApiAddr = "https://static-maps.yandex.ru/1.x/?"
+games = {}
+
+class GameInstance:
+    def __init__(self) -> None:
+        self.hint = []
+        self.reset()
+
+    def reset(self):        
+        slidePaths = []
+        chosenCity =  random.choice(cities)
+        chosenCity = chosenCity
+        hint = cities
+        hintNot = []
+        cityCoords = fetch_coordinates(apikey,chosenCity)
+        cityCoords = [ float(cityCoords[0]), float(cityCoords[1]) ]
+        hintsUsed = 0
+
+        slideCount = 10
+        for i in range(0,slideCount):
+            zoom = int(random.uniform(13,17))
+            offset = [ random.uniform(-0.05,0.05), random.uniform(-0.05,0.05) ]
+            pos = [ cityCoords[0] + offset[0], cityCoords[1] + offset[1] ]
+            mtype = random.choice(['map','sat'])
+            args = {
+                'l' : mtype,
+                'll' : f"{pos[0]},{pos[1]}",
+                'z' : str(zoom)
+            }
+            slidePath = staticApiAddr + encodeParams(args)
+            slidePaths.append(slidePath)
+        
+        self.slidePaths = slidePaths,
+        self.chosenCity = chosenCity,
+        self.hint = []
+        self.hint = hint,
+        self.hintNot = hintNot,
+        self.cityCoords = cityCoords,
+        self.hintsUsed = hintsUsed
+
+    def getHint(self):
+        if len(self.hint) > 2:
+            el = random.choice(self.hint)
+            self.hintNot.append(el)
+            self.hint.remove(el)
+            self.hintsUsed += 1
+    
 
 def fetch_coordinates(apikey, place):
     base_url = "https://geocode-maps.yandex.ru/1.x"
@@ -39,38 +83,7 @@ def slide(slide_number):
 @app.route('/reset')
 def reset():
     clientAddr = request.environ["REMOTE_ADDR"]
-    slidePaths = []
-    chosenCity =  random.choice(cities)
-    chosenCity = chosenCity
-    hint = cities
-    hintNot = []
-    cityCoords = fetch_coordinates(apikey,chosenCity)
-    cityCoords = [ float(cityCoords[0]), float(cityCoords[1]) ]
-    hintsUsed = 0
-
-    slideCount = 10
-    for i in range(0,slideCount):
-        zoom = int(random.uniform(13,17))
-        offset = [ random.uniform(-0.05,0.05), random.uniform(-0.05,0.05) ]
-        pos = [ cityCoords[0] + offset[0], cityCoords[1] + offset[1] ]
-        mtype = random.choice(['map','sat'])
-        args = {
-            'l' : mtype,
-            'll' : f"{pos[0]},{pos[1]}",
-            'z' : str(zoom)
-        }
-        slidePath = staticApiAddr + encodeParams(args)
-        slidePaths.append(slidePath)
-
-    print("Игра сброшена. Новый город:",chosenCity)
-    games[clientAddr] = {
-        'slidePaths': slidePaths,
-        'chosenCity': chosenCity,
-        'hint': hint,
-        'hintNot': hintNot,
-        'cityCoords': cityCoords,
-        'hintsUsed': hintsUsed,
-    }
+    games[clientAddr] = GameInstance()
     return redirect('/game')
 
 @app.route("/game")
@@ -80,11 +93,7 @@ def game():
 @app.route('/hint/<int:slide_number>')
 def hintFunc(slide_number):
     clientAddr = request.environ["REMOTE_ADDR"]
-    if len(games[clientAddr].hint) > 2:
-        el = random.choice(games[clientAddr].hint)
-        games[clientAddr].hintNot.append(el)
-        games[clientAddr].hint.remove(el)
-        games[clientAddr].hintsUsed += 1
+    games[clientAddr].getHint()
     return redirect(f'/slide/{slide_number}')
 
 @app.route('/answer/<int:slideNum>', methods = ['POST'])
